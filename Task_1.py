1f62dd2b52e2e7cf06f3a72fdebdd06108a72429
MichaelKupershmidt <kupershmidtmichael@gmail.com>
2022-05-28 04:16:58 -0400

    level 2 works successfuly, need to parse json for level 3

--- Task_1.py
+++ Task_1.py
@@ -1,44 +1,78 @@
 import flask
-from flask import jsonify
+from flask import jsonify, abort, request, make_response
+import json
+def Convert(lst):
+    res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
+    return res_dct
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
-
-@app.route('/healthz')
-def healthz():
-    return ''
-
-
-@app.route("/accounts/<id>")
-def accounts(id):
-    ih = int(id)
-    d = {
+d = {
   "accounts": [
     { "id": "0", "firstname": "Pooh", "lastname": "Shiesty" },
     { "id": "1", "firstname": "Albert", "lastname": "Einstein" },
     { "id": "2", "firstname": "Vitalik", "lastname": "Buterin" }
   ]
-}
-    if not ih>-1:
-        return jsonify(d)
-    elif not ih in d['accounts']:
-        return "",404
-    else: 
+    }
+
+@app.route('/healthz')
+def healthz():
+    return ''
+
+@app.route("/accounts")
+def accounts_no_id():
+    return jsonify(d)
+
+@app.route("/accounts/<id>")
+def accounts(id):
+    try:
+        ih = int(id)
         j = d['accounts'][ih]
         return jsonify(j)
+    except Exception:
+        raise abort(404)
+@app.route("/accounts/<id>/update", methods=['POST'])
+def accounts_update(id):
+    if request.method == 'POST':
+        try:
+            data = request.get_json(force=True)
+            data = Convert(data)
+            ih = int(id)
+            j = d['accounts'][ih]
+        except Exception:
+            raise abort(404)
+        try:
+            for key in j.keys():
+                j[key] = data[key]
+            d['accounts'][ih] = j
+            return make_response(jsonify(d), 200)
+
+        except Exception:
+            raise abort(400)
+
+@app.route('/post', methods=['POST'])
+def post_route():
+    if request.method == 'POST':
+
+        data = dict(request.get_json(force=True))
+        #data.json()
+        data = Convert(data)
+        #print('Data Received: "{data}"'.format(data=data))
+        return "Request Processed.\n"
 
 app.run(port=8080)
 
 
+# curl localhost:8080/accounts/0 
 
-#not sure if this is also potential answer for level 2
-    # try:
-    #     ih = int(id)
-        
-    #     if not ih>-1:
-    #         return jsonify(d)
-    #     else: 
-    #         j = d['accounts'][ih]
-    #         return jsonify(j)
-    # except Exception:
-    #     raise abort(404)
+# curl -v -X POST localhost:8080/accounts/0/update -d '{ "abc": "xyz" } 
+
+# curl -v -X POST localhost:8080/accounts/0/update -d "{ \"firstname\": \"John\", \"lastname\": \"Smith\" }" 
+
+# curl localhost:8080/accounts/0 
+
+# curl -v -X POST localhost:8080/accounts/0/update -d '{ "firstname": "Jane" } 
+
+# curl localhost:8080/accounts/0 
+# curl -v -X POST localhost:8080/accounts/0/update -d '{ "lastname": "Doe" }
+
+# curl localhost:8080/accounts/0 
