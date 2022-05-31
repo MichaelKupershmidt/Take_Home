dd00d844fddfa2f53821c3935d5b49818fe4f903
MichaelKupershmidt <kupershmidtmichael@gmail.com>
2022-05-31 17:08:57 -0400

    simpler level 2

--- task_2.py
+++ task_2.py
@@ -1,28 +1,8 @@
-#returns last 10 digits of a^b
-def last(z):
-	res = 1     
-	x = z
-	y = z
-
-	x = x % 10000000000
-	if (x == 0):
-		return 0
-	while (y > 0) :
-		if ((y%2) == 1):
-			res = (res * x) % 10000000000
-		y = int(y/2)
-		x = (x * x) % 10000000000
-	return res
-# driver code
-# 
-# 
-# 
-# 
 answer = 1000
 n=0
 p=1
 while p<(answer+1):
-	n = n + last(p)
+	n = n + pow(p,p,10000000000)
 	n= n % 10000000000
 	p+=1
 print(n)
