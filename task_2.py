591509155cac62f5e70faebaeef354635fb316f6
MichaelKupershmidt <kupershmidtmichael@gmail.com>
2022-05-31 16:41:12 -0400

    task 2

--- task_2.py
+++ task_2.py
@@ -1,33 +1,28 @@
-#returns last 5 digits of a^b
-#def last(n):
-# need to calulate eulers thoerm and get totient function
-# also need to code gcd function and neew phi function
-# solving for mod 100000 is a given and therfore not a parameter
-# number is raised to itself therfore not a parameter 
-# need to solve follwing equations/ implement functions:
-# a^x mod m = a^(x mod t) mod m
-# t = totient(m)
-# gcd(a,b)=1
-# φ(a)
+#returns last 10 digits of a^b
+def last(z):
+	res = 1     
+	x = z
+	y = z
 
-def calculate(endpoint, prevnum=0, n=0):
-	if n == endpoint:
-		if endpoint<11:
-			return prevnum + n**n
-		else:
-			return prevnum + last(n)
-	if n<6: #because before 6 there are no numbers with over 5 digits
-		prevnum = prevnum + n**n
-		return calculate(endpoint,prevnum,n+1)
-	else:
-		square = last(n)
-		prevnum = (prevnum + square)
-		return calculate(endpoint,prevnum,n+1)
-
+	x = x % 10000000000
+	if (x == 0):
+		return 0
+	while (y > 0) :
+		if ((y%2) == 1):
+			res = (res * x) % 10000000000
+		y = int(y/2)
+		x = (x * x) % 10000000000
+	return res
 # driver code
 # 
 # 
 # 
 # 
-answer = calculate(5)
-print(answer)
+answer = 1000
+n=0
+p=1
+while p<(answer+1):
+	n = n + last(p)
+	n= n % 10000000000
+	p+=1
+print(n)
